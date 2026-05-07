53de7720cf2c780a8f57cb8c966637b5c64483f0
mmidtbo <mauza628@gmail.com>
2026-05-07 21:44:35 +0700

    feat(ts): implement enhanced Todo List with date support and TypeScript safety

---
+++ script.ts
@@ -0,0 +1,86 @@
+const inputElement = document.getElementById("inputTask");
+const inputDate = document.getElementById("inputDate");
+const btnTambah = document.getElementById("btnTambahTodo");
+const btnHapus = document.getElementById("btnHapusTodo");
+const btnEdit = document.getElementById("btnEditTodo");
+const daftarTugas = document.getElementById("listTugas");
+
+if (btnTambah) {
+  btnTambah.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (
+      !inputElement ||
+      inputElement.value == "" ||
+      !inputDate ||
+      inputDate.value == ""
+    ) {
+      alert("Input tidak boleh kosong");
+      return;
+    }
+
+    const newList = document.createElement("li");
+    const text = document.createElement("p");
+    const date = document.createElement("p");
+    text.innerHTML = inputElement.value;
+    date.innerHTML = inputDate.value;
+    console.log(inputDate.value);
+
+    newList.appendChild(text);
+    newList.appendChild(date);
+
+    if (!daftarTugas) {
+      alert("daftar tugas tidak ada");
+      return;
+    }
+
+    // console.log(typeof daftarTugas.children.length);
+    // console.log(daftarTugas.children.length);
+    // console.log(daftarTugas.children[0]);
+
+    daftarTugas.appendChild(newList);
+
+    inputElement.value = "";
+  });
+}
+
+if (btnHapus) {
+  btnHapus.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (inputElement.value === "" || !inputElement) {
+      alert("input kosong");
+      return;
+    }
+
+    const node = daftarTugas?.children[inputElement.value - 1];
+    console.log(node);
+
+    if (node === undefined) {
+      alert("node undefined");
+      inputElement.value = "";
+      return;
+    }
+
+    if (!daftarTugas?.children.length) {
+      alert("daftar tugas kosong");
+      inputElement.value = "";
+      return;
+    }
+
+    daftarTugas.removeChild(node);
+
+    inputElement.value = "";
+  });
+}
+
+if (btnEdit) {
+  btnEdit.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (!daftarTugas) {
+      return;
+    }
+  });
+}
+
+// btnEdit.addEventListener("click", function () {});
